c349f45919a6f880a63169a9e58120f80f4bc623
Terram3 <ananasen@live.se>
2025-07-16 11:58:29 +0200

    bugfix

--- day6/day6-tuning.py
+++ day6/day6-tuning.py
@@ -1,7 +1,7 @@
 def main():
     with open("comp.txt", "r") as f:
         text: str = f.readline()
-        print(solve3(text, counter=0))
+        print(solve3(text))
 
 def solve(text: str, counter: int) -> int:
     if(len(text) < 4):
@@ -13,14 +13,14 @@ def solve(text: str, counter: int) -> int:
         counter += 1
         return solve(text[1:], counter=counter)
 
-def solve2(text: str, counter: int) -> int:
+def solve2(text: str) -> int:
     for i in range(len(text)-3):
         noLogic: set[str] = set(text[i:i+4])
         if len(noLogic) == 4:
             return i+4
     return -1
 
-def solve3(text: str, counter: int) -> int:
+def solve3(text: str) -> int:
     for i in range(len(text)-13):
         noLogic: set[str] = set(text[i:i+14])
         if len(noLogic) == 14:
