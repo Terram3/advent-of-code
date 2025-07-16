cb4fbdbfd521aca9e4acfe51629349a96e62a9f0
Terram3 <ananasen@live.se>
2025-07-16 12:01:07 +0200

    Small fix

--- day6/day6-tuning.py
+++ day6/day6-tuning.py
@@ -1,7 +1,7 @@
 def main():
     with open("comp.txt", "r") as f:
         text: str = f.readline()
-        print(solve3(text))
+        print(solve2(text, 4))
 
 def solve(text: str, counter: int) -> int:
     if(len(text) < 4):
@@ -13,18 +13,11 @@ def solve(text: str, counter: int) -> int:
         counter += 1
         return solve(text[1:], counter=counter)
 
-def solve2(text: str) -> int:
-    for i in range(len(text)-3):
-        noLogic: set[str] = set(text[i:i+4])
-        if len(noLogic) == 4:
-            return i+4
-    return -1
-
-def solve3(text: str) -> int:
-    for i in range(len(text)-13):
-        noLogic: set[str] = set(text[i:i+14])
-        if len(noLogic) == 14:
-            return i+14
+def solve2(text: str, uniqueMessage: int) -> int:
+    for i in range(len(text)-uniqueMessage+1):
+        noLogic: set[str] = set(text[i:i+uniqueMessage])
+        if len(noLogic) == uniqueMessage:
+            return i+uniqueMessage
     return -1
 
 if __name__ == "__main__":
