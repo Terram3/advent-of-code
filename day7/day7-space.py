d4a71e9c9410653302e146b60f74a7bac6bcd62b
Terram3 <ananasen@live.se>
2025-07-16 15:54:07 +0200

    done day7

--- day7/day7-space.py
+++ day7/day7-space.py
@@ -8,20 +8,15 @@ def solve(limit: int, d: dict[str, int]):
                 newDict[itemI] += d[itemJ]
         if newDict[itemI] <= limit:
             result += newDict[itemI]
-    d = newDict
-    print(d)
     print(result)
+    newSpace: int = 30000000-(70000000-newDict["/()"])
+    solve2(limit=newSpace, d=newDict)
 
 def solve2(limit: int, d: dict[str, int]):
-    result: int = 0
-    newDict: dict[str, int] = d.copy()
-    for itemI in d:
-        for itemJ in d:
-            if itemJ.startswith(itemI) and itemI != itemJ:
-                newDict[itemI] += d[itemJ]
-        if newDict[itemI] >= limit and result < newDict[itemI]:
-            result = newDict[itemI]
-    print(newDict)
+    result: int = 70000000
+    for item in d:
+        if d[item] > limit and result > d[item]:
+            result = d[item]
     print(result)
 
 def main():
@@ -33,6 +28,7 @@ def main():
             if line[0] == "$":
                 if line[2:4] == "cd" and notIntialized:
                     temp = line[5:].strip() + "()"
+                    d[temp] = 0
                     notIntialized = False
                 elif line[2:4] == "cd" and line[5:7] == "..":
                     if(temp.rfind("/", 0, len(temp)-1) == 0):
@@ -49,6 +45,6 @@ def main():
                     d[temp] = int(arr[0])
                 else:
                     d[temp] = d[temp] + int(arr[0])
-        solve(limit=30000000, d=d)
+        solve(limit=100000, d=d)
 if __name__ == "__main__":
     main()
